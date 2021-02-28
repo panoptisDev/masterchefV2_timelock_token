eb4ae5bcc4ed692fd0d58becbff77208c86bd733
PikachuDefi <pikachudefi-github-3GF3PlHQGJcGhPSglwQz@schildmann.net>
2021-02-28 17:58:28 +0100

    Sun Feb 28 05:58:28 PM CET 2021

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
 const PikachuToken = artifacts.require("PikachuToken");
 const Timelock = artifacts.require("Timelock");
 const Multicall = artifacts.require("Multicall");
-const MasterChef = artifacts.require("MasterChefV2");
+const MasterChefV2 = artifacts.require("MasterChefV2");
 
 module.exports = async function (deployer) {
   await deployer.deploy(PikachuToken);
   await deployer.deploy(Timelock, "0xb7cc2059dd1040dbd82be9feb862306b20cf7e54", 86400);
-  await deployer.deploy(MasterChef, PikachuToken.address, "0xb7cc2059dd1040dbd82be9feb862306b20cf7e54", "0xb7cc2059dd1040dbd82be9feb862306b20cf7e54", 1000000000000000000, 0);
+  await deployer.deploy(MasterChefV2, PikachuToken.address, "0xb7cc2059dd1040dbd82be9feb862306b20cf7e54", "0xb7cc2059dd1040dbd82be9feb862306b20cf7e54", 1000000000000000000, 0);
 };
--- migrations/3_mint_and_transfer.js
+++ migrations/3_mint_and_transfer.js
@@ -1,12 +1,12 @@
 const PikachuToken = artifacts.require("PikachuToken");
 const Timelock = artifacts.require("Timelock");
 const Multicall = artifacts.require("Multicall");
-const MasterChef = artifacts.require("MasterChefV2");
+const MasterChefV2 = artifacts.require("MasterChefV2");
 
 module.exports = async function (deployer) {
   let time = await Timelock.deployed();
   let pika = await PikachuToken.deployed();
-  let chef = await MasterChef.deployed();
+  let chef = await MasterChefV2.deployed();
 
   pika.mint("100000000000000000000000");
   //pika.transferOwnership(chef.address);
